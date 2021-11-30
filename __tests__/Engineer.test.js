8da8e5c5281de1b2ef91e4a502587a4862504682
Monika Kothari <mkothari.1993@gmail.com>
2021-11-30 01:00:54 -0600

    added failing Engineer test and passed Engineer test

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,22 @@
+const Engineer = require('../lib/Engineer')
+
+test("creates an engineer object", () => {
+    const engineer = new Engineer('Monika', 30, 'monika.kothari@gmail.com', 'mkothari93');
+    expect(engineer.name).toEqual(expect.any(String));
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toEqual(expect.any(String));
+    expect(engineer.github).toEqual(expect.any(String));
+})
+
+test("gets Engineer github username", () => {
+    const engineer = new Engineer('Monika', 30, 'monika.kothari@gmail.com', 'mkothari93');
+    
+    expect(engineer.getGithub()).toEqual(expect.any(String));
+})
+
+
+test("gets Engineer role", () => {
+    const engineer = new Engineer('Monika', 30, 'monika.kothari@gmail.com', 'mkothari93');
+    
+    expect(engineer.getRole()).toEqual("Engineer");
+})
